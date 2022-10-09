2e7136e2f4df603524f2430f2d5828ebbf132882
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-10-10 01:41:01 +0200

    Initial commit

---
+++ setteg.js
@@ -0,0 +1,9 @@
+const MAX=5
+const namess="Humble Beings Collective";
+const discord = 'https://discord.gg/humblebeingsco';
+const twitter = 'https://twitter.com/humblebeingsco';
+const startCounter = 1471;
+const endCounter = 3291;
+const IMGPrice=0.005;
+const ADDRESS = '0xB76e1BF26921Ba7fAF282E1D93a753B7Fb8d86b1';
+
