efaeb3419d800cf58d60da3929c0916b86e88bbf
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-10-29 00:16:37 +0200

    rengape

--- setteg.js
+++ setteg.js
@@ -1,9 +1,9 @@
 const MAX=5
-const namess="Mutant y00ts Yacht Club";
-const discord = 'https://discord.gg/myyc';
-const twitter = 'https://twitter.com/myyc';
+const namess="Rengape";
+const discord = 'https://discord.gg/rengapeinc';
+const twitter = 'https://twitter.com/rengapeinc';
 const startCounter = 2836;
 const endCounter = 3333;
 const IMGPrice=0.005;
-const ADDRESS = '0xa29bc045149A7FBf3434081A870A103507CdCa9b';
+const ADDRESS = '0x518c6de9a9Fc4E4eE317b817Dd1eeE3fb7bca138';
 
