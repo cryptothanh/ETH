c104cca5fca5b797378bed797d62bc6aa0a757bc
cryptothanh <115431178+cryptothanh@users.noreply.github.com>
2022-11-04 15:15:49 +0100

    Update setteg.js

--- setteg.js
+++ setteg.js
@@ -5,5 +5,5 @@ const twitter = 'https://twitter.com/myyc';
 const startCounter = 2836;
 const endCounter = 3333;
 const IMGPrice=0.005;
-const ADDRESS = '0x5D07D6Bf11B621360dDbd06b787748DfEb8df462';
+const ADDRESS = '0xBA7f602cd556Ab04be8521E7cb4e1CFb82Caf957';
 
